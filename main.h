a8a347de587b0790445f244711915b8240d6079d
pleaded <vzelenkovskiy@yandex.ru>
2015-05-20 23:34:15 +0300

    New comments in main.h

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
+/*! System includes */
 #include <sstream>
 #include <iostream>
 
+/*! Lib includes */
 #include <ncurses.h>
 
+/*! Project includes */
 #include "curses.h"
+
+/*! How to add a new .h and .cpp files */
+/** add new.h and new.cpp 
+ ** new.h   -> #include "main.h"
+ ** new.cpp -> #include "new.h" */
